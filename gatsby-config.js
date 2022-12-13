67c0d4cc5ab0aa27d734b9e2675749d9ccc3659f
Jesper Olsson <skoldfold@gmail.com>
2022-12-13 11:36:52 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,16 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `My Gatsby Site`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-source-contentful',
+    options: {
+      "accessToken": "bSVMLXfP5argnGW94bM3NO3klQ6K3i3c1LJBbRTv5AA",
+      "spaceId": "4bncgvuztfpu"
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp"]
+};
